9193aead8c0586f96faadff1959e3cec0c227e72
Shreyansh Raj <shreyansh.raj@tothenew.com>
2022-02-17 23:04:47 +0530

    Initial Commit

---
+++ John JacobsUITests/John_JacobsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  John_JacobsUITestsLaunchTests.swift
+//  John JacobsUITests
+//
+//  Created by Shreyansh Raj Keshri on 17/02/22.
+//
+
+import XCTest
+
+class John_JacobsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
